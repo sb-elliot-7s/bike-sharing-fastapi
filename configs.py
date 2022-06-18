44b30b5dedbdc1f4d0bea6747698d81a1b924a85
Sergey Babichev <s.babichevv91p@gmail.com>
2022-06-18 19:06:43 +0300

    fix main.py and add weather_url, weather_api_key fields in configs.py

--- configs.py
+++ configs.py
@@ -6,10 +6,12 @@ class Configs(BaseSettings):
     algorithm: str
     exp_time: int
 
+    weather_url: str
+    weather_api_key: str
+
     class Config:
         env_file = '.env'
         env_file_encoding = 'utf-8'
 
 
-def get_configs() -> Configs:
-    return Configs()
+def get_configs() -> Configs: return Configs()
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from bike.controllers import bike_router
+from bike.bike_controllers import bike_router
+from bike.station_controllers import station_router
 from account.controllers import account_router
-from address.controllers import address_router
 from trip.controllers import trip_router
 
 app = FastAPI(title='bike')
 
 app.include_router(bike_router)
 app.include_router(account_router)
-app.include_router(address_router)
 app.include_router(trip_router)
+app.include_router(station_router)
