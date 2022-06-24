02570a3e0f226e765114a98d4263f0a729bc0dc5
Sergey Babichev <s.babichevv91p@gmail.com>
2022-06-24 20:51:12 +0300

    add refund and payment controllers

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bike.bike_controllers import bike_router
 from bike.station_controllers import station_router
 from account.controllers import account_router
 from trip.controllers import trip_router
+from payments_service.payment_controllers import payment_router
 
 app = FastAPI(title='bike')
 
@@ -10,3 +11,4 @@ app.include_router(bike_router)
 app.include_router(account_router)
 app.include_router(trip_router)
 app.include_router(station_router)
+app.include_router(payment_router)
