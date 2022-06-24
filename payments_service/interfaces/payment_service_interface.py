e5b975469e2b825a632aa9be6723d93ef99e5a88
Sergey Babichev <s.babichevv91p@gmail.com>
2022-06-24 20:24:39 +0300

    change payment interface

--- payments_service/interfaces/payment_service_interface.py
+++ payments_service/interfaces/payment_service_interface.py
@@ -1,8 +1,27 @@
 from abc import ABC, abstractmethod
+from typing import Optional
+
 from ..schemas import PaymentDataSchema
 
 
 class PaymentServiceInterface(ABC):
     @abstractmethod
-    async def pay_for_the_service(self, payment_data: PaymentDataSchema):
+    async def create_payment(self, email: Optional[str], phone: Optional[int],
+                             payment_data: PaymentDataSchema):
         pass
+
+    @staticmethod
+    @abstractmethod
+    async def confirm_payment(payment_id: str): pass
+
+    @staticmethod
+    @abstractmethod
+    async def cancel_payment(payment_id: str): pass
+
+    @staticmethod
+    @abstractmethod
+    async def get_payment_info(payment_id: str): pass
+
+    @staticmethod
+    @abstractmethod
+    async def get_payments_by_filter(_filter: dict): pass
